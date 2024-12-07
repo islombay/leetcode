7e1f2f2ec9588c1ba3eccc2206bc7be6cea1e314
Islombay <a.islombay@yandex.ru>
2024-12-08 01:20:37 +0500

    solve 104 with updated version

---
+++ 104-maximum-depth-of-binary-tree/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "container/list"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	queue := list.New()
+	queue.PushBack(root)
+	level := 0
+
+	for queue.Len() > 0 {
+		n := queue.Len()
+		for i := 0; i < n; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		level++
+	}
+	return level
+}
